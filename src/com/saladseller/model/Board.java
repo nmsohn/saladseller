43cdefc1fe6e86949d44b7392b16d78f7a7a9d2f
nmsohn <odna425@gmail.com>
2017-06-02 10:41:43 +0900

    Board dao/impl updated Category dao/impl updated

--- src/com/saladseller/model/Board.java
+++ src/com/saladseller/model/Board.java
@@ -6,19 +6,39 @@ import java.sql.Date;
  * Created by 5CLASS-184 on 2017-05-29.
  */
 public class Board {
-    private int b_id;
+    private int b_num;
+    private int cat;
     private String title;
     private String content;
     private Date genDate;
     private int viewCount;
+    private String spec;
     private String imgPath;
+    public Board(){}
+    public Board(int b_num,String title,String content,Date genDate,int viewCount,String spec,String imgPath, int cat){
+        this.b_num = b_num;
+        this.title = title;
+        this.content = content;
+        this.genDate = genDate;
+        this.viewCount = viewCount;
+        this.spec = spec;
+        this.imgPath = imgPath;
+        this.cat = cat;
 
-    public int getB_id() {
-        return b_id;
+    }
+    public int getB_num() {
+        return b_num;
+    }
+    public void setB_num(int b_num) {
+        this.b_num = b_num;
     }
 
-    public void setB_id(int b_id) {
-        this.b_id = b_id;
+    public int getCat() {
+        return cat;
+    }
+
+    public void setCat(int cat) {
+        this.cat = cat;
     }
 
     public String getTitle() {
@@ -51,6 +71,14 @@ public class Board {
 
     public void setViewCount(int viewCount) {
         this.viewCount = viewCount;
+    }
+
+    public String getSpec() {
+        return spec;
+    }
+
+    public void setSpec(String spec) {
+        this.spec = spec;
     }
 
     public String getImgPath() {
